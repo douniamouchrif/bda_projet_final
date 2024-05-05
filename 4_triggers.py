da8a824f0a48e19d152219eb24dc576048ee02a2
douniamouchrif <102798610+douniamouchrif@users.noreply.github.com>
2024-05-05 13:46:37 +0200

    Update 4_triggers.py

--- 4_triggers.py
+++ 4_triggers.py
@@ -16,5 +16,24 @@ cursor.execute(query6)
 #Ajoutez un trigger qui utilise la procédure stockée précédente pour mettre à jour 
 #la population d'un département/région quand la population d'une ville est mise à jour.
 
+query7 = """
+CREATE OR REPLACE FUNCTION maj_pop()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF TG_TABLE_NAME = 'Pop_Commune' THEN
+        PERFORM calcul_pop_dep_reg2();
+    END IF;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_maj_pop
+AFTER INSERT OR UPDATE OR DELETE ON Pop_Commune
+FOR EACH STATEMENT EXECUTE FUNCTION maj_pop();
+"""
+cursor.execute(query7) 
+
+## jsp comment tester si ça marche genre il faudrait faire une modif et voir si ça fait bien les maj ??
+
 conn.commit()
 conn.close()
