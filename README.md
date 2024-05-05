840a56452233efbf9760f36a251950f368b495fa
mathildetissandier <102798509+mathildetissandier@users.noreply.github.com>
2024-05-05 16:27:22 +0200

    Revert "renommage"
    This reverts commit 180c771927146e7ddd0648d3ac3b56ee169e8668.

--- README.md
+++ README.md
@@ -39,7 +39,7 @@ drop table stats_mar1 ;
 drop table stats_mar2 ;
 drop table stats_mar3 ;
 drop table stats_mar4 ;
-drop table pop_commune CASCADE;
+drop table pop_commune;
 drop table stats_var ;
 DROP TABLE commune CASCADE;
 drop table departement;
--- question3.py
+++ question3.py
@@ -2,10 +2,10 @@ from datas.connect import conn
 
 cursor = conn.cursor()
 
-# N'oubliez pas de modifier au préalable la structure de la base pour accueillir ces nouvelles informations.
-# (sans utiliser la commande drop table)
+#N'oubliez pas de modifier au préalable la structure de la base pour accueillir ces nouvelles informations.
+#(sans utiliser la commande drop table)
 
-# à executer une fois si on lance plusieurs fois le fichier le mettre en commentaire pour tester les versions
+#à executer une fois si on lance plusieurs fois le fichier le mettre en commentaire pour tester les versions
 
 pop_dep = """ALTER TABLE Departement 
 ADD COLUMN pop2020 BIGINT,
@@ -32,41 +32,18 @@ cursor.execute(pop_reg)
 
 # Écrivez une procédure stockée qui fait ce calcul à partir de la population des communes.
 
-# VERSION 1 : avec l'utilisation des vues créées
-
-query1 = """CREATE OR REPLACE PROCEDURE calcul_pop_dep_reg1()
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    UPDATE Departement d
-    SET pop2020 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'P20_POP'),
-        pop2014 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'P14_POP'),
-        pop2009 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'P09_POP'),
-        pop1999 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'D99_POP'),
-        pop1990 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'D90_POP'),
-        pop1982 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'D82_POP'),
-        pop1975 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'D75_POP'),
-        pop1968 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'D68_POP');
-
-    UPDATE Region r
-    SET pop2020 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'P20_POP'),
-        pop2014 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'P14_POP'),
-        pop2009 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'P09_POP'),
-        pop1999 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'D99_POP'),
-        pop1990 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'D90_POP'),
-        pop1982 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'D82_POP'),
-        pop1975 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'D75_POP'),
-        pop1968 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'D68_POP');
-
-END;
-$$;"""
-cursor.execute(query1)
-cursor.execute("""CALL calcul_pop_dep_reg1()""")
+##JSP SI ON PEUT RÉUTILISER LA VUE DE LA QUESTION PRECEDENTE C'EST PLUS RAPIDE ET SIMPLE SI OUI 
+##J'AVAIS JUSTE UN DOUTE SUR SI LA VUE SE MET À JOUR SI ON CHANGE LES TABLES ?? 
+#genre si on change la pop d'une commune est ce que la vue le saura ? (en soit faut juste la relancer et elle se met à jour ?) 
+#j'ai essayé de faire les deux versions !! 
+#les deux donne le meme resultats 
+## pour tester une version mettre l'autre en commenaire 
+#et pour voir si ça a marcher dans terminale faire select * from region; ou departement
 
 
-# VERSION 2 : sans l'utilisation des vues créées
+####### VERSION 1 sans vues 
 
-'''query2 = """CREATE OR REPLACE PROCEDURE calcul_pop_dep_reg2()
+'''query3 = """CREATE OR REPLACE PROCEDURE calcul_pop_dep_reg2()
 LANGUAGE plpgsql
 AS $$
 BEGIN
@@ -92,8 +69,39 @@ BEGIN
 
 END;
 $$;"""
-cursor.execute(query2)
-cursor.execute("""CALL calcul_pop_dep_reg2()""")'''
+cursor.execute(query3)
+cursor.execute("""CALL calcul_pop_dep_reg()""")'''
+
+####### VERSION 2 avec vues
+
+query4 = """CREATE OR REPLACE PROCEDURE calcul_pop_dep_reg2()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    UPDATE Departement d
+    SET pop2020 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'P20_POP'),
+        pop2014 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'P14_POP'),
+        pop2009 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'P09_POP'),
+        pop1999 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'D99_POP'),
+        pop1990 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'D90_POP'),
+        pop1982 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'D82_POP'),
+        pop1975 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'D75_POP'),
+        pop1968 = (SELECT SUM(population) FROM Pop_Dep WHERE num_departement = d.num_dep AND id_stat = 'D68_POP');
+
+    UPDATE Region r
+    SET pop2020 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'P20_POP'),
+        pop2014 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'P14_POP'),
+        pop2009 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'P09_POP'),
+        pop1999 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'D99_POP'),
+        pop1990 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'D90_POP'),
+        pop1982 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'D82_POP'),
+        pop1975 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'D75_POP'),
+        pop1968 = (SELECT SUM(population) FROM Pop_Reg WHERE num_region = r.num_reg AND id_stat = 'D68_POP');
+
+END;
+$$;"""
+cursor.execute(query4)
+cursor.execute("""CALL calcul_pop_dep_reg2()""")
 
 conn.commit()
 conn.close()
