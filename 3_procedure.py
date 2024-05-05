180c771927146e7ddd0648d3ac3b56ee169e8668
suhailaabarkan <102798630+suhailaabarkan@users.noreply.github.com>
2024-05-05 13:40:08 +0200

    renommage

--- 3_procedure.py
+++ 3_procedure.py
@@ -2,10 +2,10 @@ from datas.connect import conn
 
 cursor = conn.cursor()
 
-#N'oubliez pas de modifier au préalable la structure de la base pour accueillir ces nouvelles informations.
-#(sans utiliser la commande drop table)
+# N'oubliez pas de modifier au préalable la structure de la base pour accueillir ces nouvelles informations.
+# (sans utiliser la commande drop table)
 
-#à executer une fois si on lance plusieurs fois le fichier le mettre en commentaire pour tester les versions
+# à executer une fois si on lance plusieurs fois le fichier le mettre en commentaire pour tester les versions
 
 pop_dep = """ALTER TABLE Departement 
 ADD COLUMN pop2020 BIGINT,
@@ -32,49 +32,9 @@ cursor.execute(pop_reg)
 
 # Écrivez une procédure stockée qui fait ce calcul à partir de la population des communes.
 
-##JSP SI ON PEUT RÉUTILISER LA VUE DE LA QUESTION PRECEDENTE C'EST PLUS RAPIDE ET SIMPLE SI OUI 
-##J'AVAIS JUSTE UN DOUTE SUR SI LA VUE SE MET À JOUR SI ON CHANGE LES TABLES ?? 
-#genre si on change la pop d'une commune est ce que la vue le saura ? (en soit faut juste la relancer et elle se met à jour ?) 
-#j'ai essayé de faire les deux versions !! 
-#les deux donne le meme resultats 
-## pour tester une version mettre l'autre en commenaire 
-#et pour voir si ça a marcher dans terminale faire select * from region; ou departement
+# VERSION 1 : avec l'utilisation des vues créées
 
-
-####### VERSION 1 sans vues 
-
-'''query3 = """CREATE OR REPLACE PROCEDURE calcul_pop_dep_reg2()
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    UPDATE Departement d
-    SET pop2020 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'P20_POP'),
-        pop2014 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'P14_POP'),
-        pop2009 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'P09_POP'),
-        pop1999 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'D99_POP'),
-        pop1990 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'D90_POP'),
-        pop1982 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'D82_POP'),
-        pop1975 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'D75_POP'),
-        pop1968 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'D68_POP');
-
-    UPDATE Region r
-    SET pop2020 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'P20_POP'),
-        pop2014 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'P14_POP'),
-        pop2009 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'P09_POP'),
-        pop1999 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'D99_POP'),
-        pop1990 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'D90_POP'),
-        pop1982 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'D82_POP'),
-        pop1975 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'D75_POP'),
-        pop1968 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'D68_POP');
-
-END;
-$$;"""
-cursor.execute(query3)
-cursor.execute("""CALL calcul_pop_dep_reg()""")'''
-
-####### VERSION 2 avec vues
-
-query4 = """CREATE OR REPLACE PROCEDURE calcul_pop_dep_reg2()
+query1 = """CREATE OR REPLACE PROCEDURE calcul_pop_dep_reg1()
 LANGUAGE plpgsql
 AS $$
 BEGIN
@@ -100,8 +60,40 @@ BEGIN
 
 END;
 $$;"""
-cursor.execute(query4)
-cursor.execute("""CALL calcul_pop_dep_reg2()""")
+cursor.execute(query1)
+cursor.execute("""CALL calcul_pop_dep_reg1()""")
+
+
+# VERSION 2 : sans l'utilisation des vues créées
+
+'''query2 = """CREATE OR REPLACE PROCEDURE calcul_pop_dep_reg2()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    UPDATE Departement d
+    SET pop2020 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'P20_POP'),
+        pop2014 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'P14_POP'),
+        pop2009 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'P09_POP'),
+        pop1999 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'D99_POP'),
+        pop1990 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'D90_POP'),
+        pop1982 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'D82_POP'),
+        pop1975 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'D75_POP'),
+        pop1968 = (SELECT SUM(p.valeur) FROM Commune c JOIN Pop_Commune p ON c.num_com = p.num_com WHERE c.num_dep = d.num_dep AND p.id_stat = 'D68_POP');
+
+    UPDATE Region r
+    SET pop2020 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'P20_POP'),
+        pop2014 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'P14_POP'),
+        pop2009 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'P09_POP'),
+        pop1999 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'D99_POP'),
+        pop1990 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'D90_POP'),
+        pop1982 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'D82_POP'),
+        pop1975 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'D75_POP'),
+        pop1968 = (SELECT SUM(p.valeur) FROM Departement d JOIN Commune c ON d.num_dep = c.num_dep JOIN Pop_Commune p ON c.num_com = p.num_com WHERE d.num_reg = r.num_reg AND p.id_stat = 'D68_POP');
+
+END;
+$$;"""
+cursor.execute(query2)
+cursor.execute("""CALL calcul_pop_dep_reg2()""")'''
 
 conn.commit()
 conn.close()
